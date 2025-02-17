singlestore-labs/terraform-provider-singlestore

PLAT-7336 Add private_connection validation rules

internal/provider/privateconnections/privateconnectionvalidator.go
@@ -1,0 +1,101 @@
+package privateconnections
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/singlestore-labs/singlestore-go/management"
+	"github.com/singlestore-labs/terraform-provider-singlestoredb/internal/provider/util"
+)
+
+var allowListRequiredMsg = "allow_list configuration is required for INBOUND private connections."
+
+type validationRule struct {
+	condition bool
+	message   string
+}
+
+func ValidatePrivateConnection(plan PrivateConnectionModel, isUpdate bool) *util.SummaryWithDetailError {
+	privateConnectionType, err := util.PrivateConnectionTypeString(plan.Type)
+	if err != nil {
+		return &util.SummaryWithDetailError{
+			Summary: err.Error(),
+			Detail:  err.Error(),
+		}
+	}
+
+	var rules []validationRule
+
+	switch privateConnectionType {
+	case management.PrivateConnectionCreateTypeINBOUND:
+		rules = getInboundValidationRules(plan, isUpdate)
+	case management.PrivateConnectionCreateTypeOUTBOUND:
+		rules = getOutboundValidationRules(plan, isUpdate)
+	default:
+		return &util.SummaryWithDetailError{
+			Summary: "Unknown private connection type.",
+			Detail:  "Unknown private connection type.",
+		}
+	}
+
+	for _, rule := range rules {
+		if rule.condition {
+			return &util.SummaryWithDetailError{
+				Summary: "Failed to validate private connection configuration",
+				Detail:  rule.message,
+			}
+		}
+	}
+
+	return nil
+}
+
+func getInboundValidationRules(plan PrivateConnectionModel, isUpdate bool) []validationRule {
+	return []validationRule{
+		{isUpdate && isUndefined(plan.AllowList), allowListRequiredMsg},
+		{isDefined(plan.ServiceName), "service_name configuration is not allowed for INBOUND private connections."},
+		{isDefined(plan.KaiEndpointID) && isUndefined(plan.WorkspaceID), "workspace_id configuration is required for SingleStore Kai INBOUND private connections."},
+		{isUndefined(plan.KaiEndpointID) && isUndefined(plan.AllowList), allowListRequiredMsg},
+	}
+}
+
+func getOutboundValidationRules(plan PrivateConnectionModel, isUpdate bool) []validationRule {
+	return []validationRule{
+		{isUpdate, "OUTBOUND private connections update is not allowed."},
+		{isDefined(plan.AllowList), "allow_list configuration is not allowed for OUTBOUND private connections."},
+		{isUndefined(plan.ServiceName), "service_name configuration is required for OUTBOUND private connections."},
+	}
+}
+
+func ValidatePrivateConnectionMofdifyPlan(plan, state PrivateConnectionModel) *util.SummaryWithDetailError {
+	rules := []validationRule{
+		{hasChanged(plan.Type, state.Type), "Changing the type configuration is currently not supported."},
+		{hasChanged(plan.ServiceName, state.ServiceName), "Changing the service_name configuration is currently not supported."},
+		{hasChanged(plan.KaiEndpointID, state.KaiEndpointID), "Changing the kai_endpoint_id configuration is currently not supported."},
+		{hasChanged(plan.SQLPort, state.SQLPort), "Changing the sql_port configuration is currently not supported."},
+		{hasChanged(plan.WebsocketsPort, state.WebsocketsPort), "Changing the web_socket_port configuration is currently not supported."},
+		{hasChanged(plan.WorkspaceGroupID, state.WorkspaceGroupID), "Changing the workspace_group_id configuration is not supported."},
+		{hasChanged(plan.WorkspaceID, state.WorkspaceID), "Changing the workspace_id configuration is not supported."},
+	}
+
+	for _, rule := range rules {
+		if rule.condition {
+			return &util.SummaryWithDetailError{
+				Summary: "Failed to validate private connection configuration",
+				Detail:  rule.message,
+			}
+		}
+	}
+
+	return nil
+}
+
+func isDefined(field attr.Value) bool {
+	return !field.IsNull() && !field.IsUnknown()
+}
+
+func isUndefined(field attr.Value) bool {
+	return !isDefined(field)
+}
+
+func hasChanged(plan, state attr.Value) bool {
+	return !plan.IsNull() && !plan.IsUnknown() && !plan.Equal(state)
+}

internal/provider/privateconnections/privateconnectionvalidator_test.go
@@ -1,0 +1,137 @@
+package privateconnections_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/singlestore-labs/terraform-provider-singlestoredb/internal/provider/privateconnections"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidatePrivateConnection(t *testing.T) {
+	plan := privateconnections.PrivateConnectionModel{
+		Type:             types.StringValue("INBOUND"),
+		WorkspaceGroupID: types.StringValue("721cdcaf-3555-4434-8f2e-d4e77d9a5d25"),
+	}
+
+	// INBOUND
+
+	err := privateconnections.ValidatePrivateConnection(plan, false)
+	require.NotNil(t, err)
+	require.Equal(t, "allow_list configuration is required for INBOUND private connections.", err.Detail)
+
+	err = privateconnections.ValidatePrivateConnection(plan, true)
+	require.NotNil(t, err)
+	require.Equal(t, "allow_list configuration is required for INBOUND private connections.", err.Detail)
+
+	plan.AllowList = types.StringValue("12345")
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.Nil(t, err)
+
+	err = privateconnections.ValidatePrivateConnection(plan, true)
+	require.Nil(t, err)
+
+	plan.ServiceName = types.StringValue("test service name")
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.NotNil(t, err)
+	require.Equal(t, "service_name configuration is not allowed for INBOUND private connections.", err.Detail)
+
+	plan.ServiceName = types.StringNull()
+	plan.KaiEndpointID = types.StringValue("vce-test")
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.NotNil(t, err)
+	require.Equal(t, "workspace_id configuration is required for SingleStore Kai INBOUND private connections.", err.Detail)
+
+	plan.WorkspaceID = types.StringValue("vce-testfa3d7868-fd40-40b5-8001-4b73de5d94c1")
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.Nil(t, err)
+
+	// OUTBOUND
+
+	plan = privateconnections.PrivateConnectionModel{
+		Type:             types.StringValue("OUTBOUND"),
+		WorkspaceGroupID: types.StringValue("721cdcaf-3555-4434-8f2e-d4e77d9a5d25"),
+	}
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.NotNil(t, err)
+	require.Equal(t, "service_name configuration is required for OUTBOUND private connections.", err.Detail)
+
+	plan.ServiceName = types.StringValue("test service name")
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.Nil(t, err)
+
+	plan.AllowList = types.StringValue("12345")
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.NotNil(t, err)
+	require.Equal(t, "allow_list configuration is not allowed for OUTBOUND private connections.", err.Detail)
+
+	plan.AllowList = types.StringNull()
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.Nil(t, err)
+
+	err = privateconnections.ValidatePrivateConnection(plan, true)
+	require.NotNil(t, err)
+	require.Equal(t, "OUTBOUND private connections update is not allowed.", err.Detail)
+}
+
+func TestValidatePrivateConnectionModifyPlan(t *testing.T) {
+	plan := privateconnections.PrivateConnectionModel{
+		Type:             types.StringValue("INBOUND"),
+		WorkspaceGroupID: types.StringValue("721cdcaf-3555-4434-8f2e-d4e77d9a5d25"),
+		AllowList:        types.StringValue("123345"),
+	}
+
+	state := privateconnections.PrivateConnectionModel{
+		Type:             types.StringValue("INBOUND"),
+		AllowList:        types.StringValue("111111"),
+		ServiceName:      types.StringValue("service name"),
+		KaiEndpointID:    types.StringValue("kai-vcpe"),
+		SQLPort:          types.Float32Value(3306),
+		WebsocketsPort:   types.Float32Value(443.0),
+		WorkspaceGroupID: types.StringValue("721cdcaf-3555-4434-8f2e-d4e77d9a5d25"),
+		WorkspaceID:      types.StringValue("41c1c310-9a5f-4a7a-ba8e-088af6056d8d"),
+	}
+
+	err := privateconnections.ValidatePrivateConnectionMofdifyPlan(plan, state)
+	require.Nil(t, err)
+
+	plan.Type = types.StringValue("OUTBOUND")
+
+	err = privateconnections.ValidatePrivateConnectionMofdifyPlan(plan, state)
+	require.NotNil(t, err)
+	require.Equal(t, "Changing the type configuration is currently not supported.", err.Detail)
+
+	plan.Type = types.StringValue("INBOUND")
+
+	plan.ServiceName = types.StringValue("updated service name")
+
+	err = privateconnections.ValidatePrivateConnectionMofdifyPlan(plan, state)
+	require.NotNil(t, err)
+	require.Equal(t, "Changing the service_name configuration is currently not supported.", err.Detail)
+
+	plan.ServiceName = types.StringNull()
+	plan.WorkspaceID = types.StringValue("06291fc6-1dd5-495f-b9aa-90bd5961dd65")
+
+	err = privateconnections.ValidatePrivateConnectionMofdifyPlan(plan, state)
+	require.NotNil(t, err)
+	require.Equal(t, "Changing the workspace_id configuration is not supported.", err.Detail)
+
+	plan.WorkspaceID = types.StringNull()
+	plan.WorkspaceGroupID = types.StringValue("06291fc6-1dd5-495f-b9aa-90bd5961dd65")
+
+	err = privateconnections.ValidatePrivateConnectionMofdifyPlan(plan, state)
+	require.NotNil(t, err)
+	require.Equal(t, "Changing the workspace_group_id configuration is not supported.", err.Detail)
+
+	plan.WorkspaceGroupID = types.StringNull()
+	err = privateconnections.ValidatePrivateConnectionMofdifyPlan(plan, state)
+	require.Nil(t, err)
+}

singlestore-labs/terraform-provider-singlestoredb

internal/provider/privateconnections/privateconnectionvalidator.go
@@ -1,0 +1,102 @@
+package privateconnections
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/singlestore-labs/singlestore-go/management"
+	"github.com/singlestore-labs/terraform-provider-singlestoredb/internal/provider/util"
+)
+
+var allowListRequiredMsg = "allow_list configuration is required for INBOUND private connections."
+
+type validationRule struct {
+	condition bool
+	message   string
+}
+
+func ValidatePrivateConnection(plan PrivateConnectionModel, isUpdate bool) *util.SummaryWithDetailError {
+	privateConnectionType, err := util.PrivateConnectionTypeString(plan.Type)
+	if err != nil {
+		return &util.SummaryWithDetailError{
+			Summary: err.Error(),
+			Detail:  err.Error(),
+		}
+	}
+
+	var rules []validationRule
+
+	switch privateConnectionType {
+	case management.PrivateConnectionCreateTypeINBOUND:
+		rules = getInboundValidationRules(plan, isUpdate)
+	case management.PrivateConnectionCreateTypeOUTBOUND:
+		rules = getOutboundValidationRules(plan, isUpdate)
+	default:
+		return &util.SummaryWithDetailError{
+			Summary: "Unknown private connection type.",
+			Detail:  "Unknown private connection type.",
+		}
+	}
+
+	for _, rule := range rules {
+		if rule.condition {
+			return &util.SummaryWithDetailError{
+				Summary: "Failed to validate private connection configuration",
+				Detail:  rule.message,
+			}
+		}
+	}
+
+	return nil
+}
+
+func getInboundValidationRules(plan PrivateConnectionModel, isUpdate bool) []validationRule {
+	return []validationRule{
+		{isUpdate && isUndefined(plan.AllowList), allowListRequiredMsg},
+		{isDefined(plan.ServiceName), "service_name configuration is not allowed for INBOUND private connections."},
+		{isDefined(plan.KaiEndpointID) && isUndefined(plan.WorkspaceID), "workspace_id configuration is required for SingleStore Kai INBOUND private connections."},
+		{isDefined(plan.KaiEndpointID) && isDefined(plan.AllowList), "allow_list configuration is not allowed for SingleStore Kai INBOUND private connections."},
+		{isUndefined(plan.KaiEndpointID) && isUndefined(plan.AllowList), allowListRequiredMsg},
+	}
+}
+
+func getOutboundValidationRules(plan PrivateConnectionModel, isUpdate bool) []validationRule {
+	return []validationRule{
+		{isUpdate, "OUTBOUND private connections update is not allowed."},
+		{isDefined(plan.AllowList), "allow_list configuration is not allowed for OUTBOUND private connections."},
+		{isUndefined(plan.ServiceName), "service_name configuration is required for OUTBOUND private connections."},
+	}
+}
+
+func ValidatePrivateConnectionModifyPlan(plan, state PrivateConnectionModel) *util.SummaryWithDetailError {
+	rules := []validationRule{
+		{hasChanged(plan.Type, state.Type), "Changing the type configuration is currently not supported."},
+		{hasChanged(plan.ServiceName, state.ServiceName), "Changing the service_name configuration is currently not supported."},
+		{hasChanged(plan.KaiEndpointID, state.KaiEndpointID), "Changing the kai_endpoint_id configuration is currently not supported."},
+		{hasChanged(plan.SQLPort, state.SQLPort), "Changing the sql_port configuration is currently not supported."},
+		{hasChanged(plan.WebsocketsPort, state.WebsocketsPort), "Changing the web_socket_port configuration is currently not supported."},
+		{hasChanged(plan.WorkspaceGroupID, state.WorkspaceGroupID), "Changing the workspace_group_id configuration is not supported."},
+		{hasChanged(plan.WorkspaceID, state.WorkspaceID), "Changing the workspace_id configuration is not supported."},
+	}
+
+	for _, rule := range rules {
+		if rule.condition {
+			return &util.SummaryWithDetailError{
+				Summary: "Failed to validate private connection configuration",
+				Detail:  rule.message,
+			}
+		}
+	}
+
+	return nil
+}
+
+func isDefined(field attr.Value) bool {
+	return !field.IsNull() && !field.IsUnknown()
+}
+
+func isUndefined(field attr.Value) bool {
+	return !isDefined(field)
+}
+
+func hasChanged(plan, state attr.Value) bool {
+	return !plan.IsNull() && !plan.IsUnknown() && !plan.Equal(state)
+}

internal/provider/privateconnections/privateconnectionvalidator_test.go
@@ -1,0 +1,139 @@
+package privateconnections_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/singlestore-labs/terraform-provider-singlestoredb/internal/provider/privateconnections"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidatePrivateConnection(t *testing.T) {
+	plan := privateconnections.PrivateConnectionModel{
+		Type:             types.StringValue("INBOUND"),
+		WorkspaceGroupID: types.StringValue("721cdcaf-3555-4434-8f2e-d4e77d9a5d25"),
+	}
+
+	// INBOUND
+
+	err := privateconnections.ValidatePrivateConnection(plan, false)
+	require.NotNil(t, err)
+	require.Equal(t, "allow_list configuration is required for INBOUND private connections.", err.Detail)
+
+	err = privateconnections.ValidatePrivateConnection(plan, true)
+	require.NotNil(t, err)
+	require.Equal(t, "allow_list configuration is required for INBOUND private connections.", err.Detail)
+
+	plan.AllowList = types.StringValue("12345")
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.Nil(t, err)
+
+	err = privateconnections.ValidatePrivateConnection(plan, true)
+	require.Nil(t, err)
+
+	plan.ServiceName = types.StringValue("test service name")
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.NotNil(t, err)
+	require.Equal(t, "service_name configuration is not allowed for INBOUND private connections.", err.Detail)
+
+	plan.ServiceName = types.StringNull()
+	plan.KaiEndpointID = types.StringValue("vce-test")
+	plan.WorkspaceID = types.StringValue("vce-testfa3d7868-fd40-40b5-8001-4b73de5d94c1")
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.NotNil(t, err)
+	require.Equal(t, "allow_list configuration is not allowed for SingleStore Kai INBOUND private connections.", err.Detail)
+
+	plan.WorkspaceID = types.StringNull()
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.NotNil(t, err)
+	require.Equal(t, "workspace_id configuration is required for SingleStore Kai INBOUND private connections.", err.Detail)
+
+	// OUTBOUND
+
+	plan = privateconnections.PrivateConnectionModel{
+		Type:             types.StringValue("OUTBOUND"),
+		WorkspaceGroupID: types.StringValue("721cdcaf-3555-4434-8f2e-d4e77d9a5d25"),
+	}
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.NotNil(t, err)
+	require.Equal(t, "service_name configuration is required for OUTBOUND private connections.", err.Detail)
+
+	plan.ServiceName = types.StringValue("test service name")
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.Nil(t, err)
+
+	plan.AllowList = types.StringValue("12345")
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.NotNil(t, err)
+	require.Equal(t, "allow_list configuration is not allowed for OUTBOUND private connections.", err.Detail)
+
+	plan.AllowList = types.StringNull()
+
+	err = privateconnections.ValidatePrivateConnection(plan, false)
+	require.Nil(t, err)
+
+	err = privateconnections.ValidatePrivateConnection(plan, true)
+	require.NotNil(t, err)
+	require.Equal(t, "OUTBOUND private connections update is not allowed.", err.Detail)
+}
+
+func TestValidatePrivateConnectionModifyPlan(t *testing.T) {
+	plan := privateconnections.PrivateConnectionModel{
+		Type:             types.StringValue("INBOUND"),
+		WorkspaceGroupID: types.StringValue("721cdcaf-3555-4434-8f2e-d4e77d9a5d25"),
+		AllowList:        types.StringValue("123345"),
+	}
+
+	state := privateconnections.PrivateConnectionModel{
+		Type:             types.StringValue("INBOUND"),
+		AllowList:        types.StringValue("111111"),
+		ServiceName:      types.StringValue("service name"),
+		KaiEndpointID:    types.StringValue("kai-vcpe"),
+		SQLPort:          types.Float32Value(3306),
+		WebsocketsPort:   types.Float32Value(443.0),
+		WorkspaceGroupID: types.StringValue("721cdcaf-3555-4434-8f2e-d4e77d9a5d25"),
+		WorkspaceID:      types.StringValue("41c1c310-9a5f-4a7a-ba8e-088af6056d8d"),
+	}
+
+	err := privateconnections.ValidatePrivateConnectionModifyPlan(plan, state)
+	require.Nil(t, err)
+
+	plan.Type = types.StringValue("OUTBOUND")
+
+	err = privateconnections.ValidatePrivateConnectionModifyPlan(plan, state)
+	require.NotNil(t, err)
+	require.Equal(t, "Changing the type configuration is currently not supported.", err.Detail)
+
+	plan.Type = types.StringValue("INBOUND")
+
+	plan.ServiceName = types.StringValue("updated service name")
+
+	err = privateconnections.ValidatePrivateConnectionModifyPlan(plan, state)
+	require.NotNil(t, err)
+	require.Equal(t, "Changing the service_name configuration is currently not supported.", err.Detail)
+
+	plan.ServiceName = types.StringNull()
+	plan.WorkspaceID = types.StringValue("06291fc6-1dd5-495f-b9aa-90bd5961dd65")
+
+	err = privateconnections.ValidatePrivateConnectionModifyPlan(plan, state)
+	require.NotNil(t, err)
+	require.Equal(t, "Changing the workspace_id configuration is not supported.", err.Detail)
+
+	plan.WorkspaceID = types.StringNull()
+	plan.WorkspaceGroupID = types.StringValue("06291fc6-1dd5-495f-b9aa-90bd5961dd65")
+
+	err = privateconnections.ValidatePrivateConnectionModifyPlan(plan, state)
+	require.NotNil(t, err)
+	require.Equal(t, "Changing the workspace_group_id configuration is not supported.", err.Detail)
+
+	plan.WorkspaceGroupID = types.StringNull()
+	err = privateconnections.ValidatePrivateConnectionModifyPlan(plan, state)
+	require.Nil(t, err)
+}